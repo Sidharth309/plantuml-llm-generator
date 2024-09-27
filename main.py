ccb3defb54073022497d0b86ed7ddfdc3669ceae
Sidharth309 <116871397+Sidharth309@users.noreply.github.com>
2024-09-27 10:42:20 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
@@ -50,5 +51,6 @@ def generate():
         "diagram_url": diagram_url
     })
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
